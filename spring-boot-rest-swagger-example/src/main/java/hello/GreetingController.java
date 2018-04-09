5b8c7a98dddeec977b4ae1ca14b860a5c1bf8d56
George Hawkins <george-hawkins@users.noreply.github.com>
2018-04-09 16:18:22 +0200

    Added in Swagger2 support.

--- spring-boot-rest-swagger-example/src/main/java/hello/GreetingController.java
+++ spring-boot-rest-swagger-example/src/main/java/hello/GreetingController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class GreetingController {
     private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @ApiOperation("Returns a greeting, optionally taking a name parameter.")
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
