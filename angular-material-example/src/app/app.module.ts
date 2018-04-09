ebc2b3f29d15bb78b6f18a6458673c0b85e56fa2
George Hawkins <george-hawkins@users.noreply.github.com>
2018-04-09 11:31:50 +0200

    Initial import of my-card.

--- angular-material-example/src/app/app.module.ts
+++ angular-material-example/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MyCardComponent } from './my-card/my-card.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCardComponent
   ],
   imports: [
     BrowserModule
